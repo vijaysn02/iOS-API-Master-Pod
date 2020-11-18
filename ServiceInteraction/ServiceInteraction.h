46a5d10c73aaa37e67e151bc6c34da5dba97b80b
Vijay <vijaysn02@gmail.com>
2020-11-18 11:01:22 +0530

    Initial Commit

---
+++ ServiceInteraction/ServiceInteraction.h
@@ -0,0 +1,19 @@
+//
+//  ServiceInteraction.h
+//  ServiceInteraction
+//
+//  Created by Sagaya Navis Vijay on 18/11/20.
+//  Copyright © 2020 vijay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ServiceInteraction.
+FOUNDATION_EXPORT double ServiceInteractionVersionNumber;
+
+//! Project version string for ServiceInteraction.
+FOUNDATION_EXPORT const unsigned char ServiceInteractionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ServiceInteraction/PublicHeader.h>
+
+
