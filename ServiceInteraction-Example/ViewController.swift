a9e7228a7ada308768eff2a79111bb1cf4cad8fd
Vijay <vijaysn02@gmail.com>
2020-11-18 11:23:45 +0530

    Initial Commit

---
+++ ServiceInteraction-Example/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  ServiceInteraction-Example
+//
+//  Created by Sagaya Navis Vijay on 18/11/20.
+//  Copyright © 2020 vijay. All rights reserved.
+//
+
+import UIKit
+
+//MARK: - View Controller - Initialisation
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        getAPICall()
+    }
+
+
+}
+
+//MARK: - Get API CAll
+extension ViewController  {
+
+    func getAPICall() {
+        
+        let urlString = "https://reqres.in/api/users"
+        let urlwithParameters = urlString + "?page=2"
+        
+        ServiceInteraction.apiCall(urlString: urlwithParameters, httpMethod: .GET, foregroundAPICall: true, parameters: nil) { (responseData) in
+            
+            print(responseData)
+            
+        }
+        
+    }
+
+}
---
+++ ServiceInteraction/ServiceInteractionHandler.swift
@@ -0,0 +1,116 @@
+//
+//  ServiceInteraction.swift
+//  iOS Starter
+//
+//  Created by Sagaya Navis Vijay on 28/09/20.
+//  Copyright © 2020 sysaxiom. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//MARK: - Service Interaction - Generic
+class ServiceInteraction {
+    
+    static func apiCall(urlString:String,httpMethod:APIMethod,foregroundAPICall:Bool,parameters:Dictionary<String, String>?,completionBlock: @escaping (Data) -> Void) -> Void
+    {
+        
+        
+        if !Reachability.isInternetAvailable() {
+            return
+        }
+        
+        let url = URL(string: urlString)
+        var request = URLRequest(url: url!)
+        request.httpMethod = httpMethod.description
+        
+        //Passing Parameter
+        if httpMethod != .GET {
+            if let param = parameters {
+                request.httpBody = try? JSONSerialization.data(withJSONObject: param, options: [])
+            } else {
+                print("Pass body parameter")
+            }
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        //request.setValue(Constants.URLS.HEADER_VALUE, forHTTPHeaderField: Constants.URLS.HEADER_KEY)
+        
+        //API Time Out
+        let session = URLSession.shared
+        session.configuration.timeoutIntervalForRequest = 30
+        session.configuration.timeoutIntervalForResource = 30
+        
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            
+            guard let responseData = data else {
+                return
+            }
+            completionBlock(responseData)
+            
+        })
+        task.resume()
+    }
+    static func apiCall(urlString:String,httpMethod:APIMethod,foregroundAPICall:Bool,parameters:Dictionary<String, String>?,completionBlock: @escaping (Data?,URLResponse?,Error?) -> Void) -> Void
+    {
+        
+        
+        if !Reachability.isInternetAvailable() {
+            return
+        }
+        
+        let url = URL(string: urlString)
+        var request = URLRequest(url: url!)
+        request.httpMethod = httpMethod.description
+        
+        //Passing Parameter
+        if httpMethod != .GET {
+            if let param = parameters {
+                request.httpBody = try? JSONSerialization.data(withJSONObject: param, options: [])
+            } else {
+                print("Pass body parameter")
+            }
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        //request.setValue(Constants.URLS.HEADER_VALUE, forHTTPHeaderField: Constants.URLS.HEADER_KEY)
+        
+        //API Time Out
+        let session = URLSession.shared
+        session.configuration.timeoutIntervalForRequest = 30
+        session.configuration.timeoutIntervalForResource = 30
+        
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            completionBlock(data,response,error)
+        })
+        task.resume()
+    }
+    
+}
+
+
+//MARK: - HTTP Methoda
+enum APIMethod {
+    
+    case GET
+    case POST
+    case PUT
+    case DELETE
+    
+    var description : String {
+     
+        switch self {
+      
+      case .GET: return "GET"
+      case .POST: return "POST"
+      case .PUT: return "PUT"
+      case .DELETE: return "DELETE"
+      }
+        
+    }
+    
+}
+
+
+
+
